a822c670be28aa5f9cdcfe22fc8b941b1d32ae32
juantonguino <juan.tonguino@gmail.com>
2016-05-30 18:22:21 -0500

    se esta organizando los procediminetos almacenados

--- app/utilities/my_personal_procedure_delete.sql
+++
@@ -1,5 +0,0 @@
-create or replace procedure insert_log_delete(user_emailv in varchar, delete_valuesv in varchar, typev in varchar)
-is
-begin
-  insert into log_delete(USER_EMAIL, DELETE_VALUES, TYPE, INSERT_AT)values(user_emailv, delete_valuesv, typev, sysdate);
-end;
