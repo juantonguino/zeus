4327f6308a04835ca0683ba653a88e2fcbad07a4
andres97473 <andres97473@hotmail.com>
2016-05-31 22:14:37 -0500

    se modifico la pagina de inicio

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -5,6 +5,6 @@
 <h1>Conoce Nariño</h1>
 <h3>El encanto está dado por su ubicación en la región Andina y en la
   ramificación de la cordillera de los Andes. Es una tierra rodeada de volcanes
-  y posee una amplia zona en la llanura costera Pacífico. Estas condiciones
+  y posee una amplia zona en la llanura y su costa Pacífica. Estas condiciones
   ofrecen diversos paisajes para disfrutar en familia</h3>
 @endsection
