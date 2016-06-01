5f016022953d9066ca9b3cc232d6070ec8c9ee09
andres97473 <andres97473@hotmail.com>
2016-05-31 22:11:42 -0500

    se modifico la pagina de inicio

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -2,4 +2,9 @@
 @section('title', 'Inicio')
 @section('title_section', 'Bienvenido '.Auth::user()->name)
 @section('content')
+<h1>Conoce Nariño</h1>
+<h3>El encanto está dado por su ubicación en la región Andina y en la
+  ramificación de la cordillera de los Andes. Es una tierra rodeada de volcanes
+  y posee una amplia zona en la llanura costera Pacífico. Estas condiciones
+  ofrecen diversos paisajes para disfrutar en familia</h3>
 @endsection
